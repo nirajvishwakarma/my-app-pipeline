de46c74a5459701f793ec033368854070aa12144
Niraj Munib Vishwakarma <47804642+nirajvishwakarma@users.noreply.github.com>
2022-05-19 21:38:51 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,4 +1,15 @@
-def function() {
+def buildApp() {
   echo "Building the application"
 }
+
+def testApp() {
+  echo "testing the application"
+}
+
+def Deployapp() {
+  echo "Deploying the application"
+  echo "deploying version ${params.VERSION}"
+}
+
+
 return this
